b7767b624f4f4c14cd0cbadd21d5d293dc076273
Nikita2341 <yuri.silenok@gmail.com>
2023-04-24 13:53:38 +0300

    first commit

---
+++ Cons1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Какой класс Вы хотите создать?");
+        switch (Console.ReadLine())
+        {
+            
+            default:
+                Console.WriteLine("Такого класса не существует");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
