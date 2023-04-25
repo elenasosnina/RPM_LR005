f999706f3929a3b975a2bea7c373dabb681461c3
Nikita2341 <yuri.silenok@gmail.com>
2023-04-25 10:20:19 +0300

    ya

--- Cons1/Creator.cs
+++ Cons1/Creator.cs
@@ -7,4 +7,44 @@ using System.Threading.Tasks;
 
 internal class ClassCreator
 {
+
+    public static Lesson Lesson()
+    {
+        return new Lesson(
+            DateTime.Now,
+            Pair(),
+            Group(),
+            Employee(),
+            TypeLesson(),
+            Discipline(),
+            Auditory());
+    }
+    public static Auditory Auditory()
+    {
+        return new Auditory();
+    }
+    public static TypeLesson TypeLesson()
+    {
+        return new TypeLesson();
+    }
+
+    public static Discipline Discipline()
+    {
+        return new Discipline();
+    }
+
+    public static Group Group()
+    {
+        return new Group();
+    }
+    
+    public static Employee Employee()
+    {
+        return new Employee();
+    }
+    
+    public static Pair Pair()
+    {
+        return new Pair();
+    }
 }
--- Cons1/Program.cs
+++ Cons1/Program.cs
@@ -7,6 +7,27 @@ while (true)
         Console.WriteLine("Какой класс Вы хотите создать?");
         switch (Console.ReadLine())
         {
+            case "Lesson":
+                UI.Print(ClassCreator.Lesson());
+                break;
+            case "Auditory":
+                UI.Print(ClassCreator.Auditory());
+                break;
+            case "Group":
+                UI.Print(ClassCreator.Group());
+                break;
+            case "Employee":
+                UI.Print(ClassCreator.Employee());
+                break;
+            case "Discipline":
+                UI.Print(ClassCreator.Discipline());
+                break;
+            case "Pair":
+                UI.Print(ClassCreator.Pair());
+                break;
+            case "TypeLesson":
+                UI.Print(ClassCreator.TypeLesson());
+                break;
             default:
                 Console.WriteLine("Такого класса не существует");
                 break;
