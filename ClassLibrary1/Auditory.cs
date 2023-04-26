aa964b135e6d353e96215e4f5fa25c6e06917eeb
y9NBA <asarkrylov76212@gmail.com>
2023-04-26 16:13:31 +0300

    byte

--- ClassLibrary1/Auditory.cs
+++ ClassLibrary1/Auditory.cs
@@ -1,23 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 //https://docs.google.com/document/d/10XwtkjNB6-lRehWP_7qbgWC1nn_oIpa28ezyPXX3B28/edit - задание﻿
 namespace ClassLibrary1
 {
     public class Auditory
     {
-        private readonly string? name; // название
-        public string? Name { get => name; }
+        private readonly string name; // название
+        public string Name { get => name; }
 
         private readonly Employee employee; // Ссылка на ответственного сотрудника
         public Employee Employee { get => employee; }
 
-        private readonly int? mest; // кол-во посадочных мест
-        public int? Mest { get => mest; }
+        private readonly byte mest; // кол-во посадочных мест
+        public byte Mest { get => mest; }
 
-        private readonly int? window; // кол-во окон
-        public int? Window { get => window; }
+        private readonly byte window; // кол-во окон
+        public byte Window { get => window; }
 
         private readonly Equipment equipment; // Ссылка на оборудование
         public Equipment Equipment { get => equipment; }
-        public Auditory(string name, Employee employee, int mest, int window, Equipment equipment) // конструктор 
+        public Auditory(string name, Employee employee, byte mest, byte window, Equipment equipment) // конструктор 
         {
             this.name = name;
             this.employee = employee;
--- ClassLibrary1/Employee.cs
+++ ClassLibrary1/Employee.cs
@@ -1,4 +1,10 @@
-﻿namespace ClassLibrary1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
 {
     public class Employee
     {
