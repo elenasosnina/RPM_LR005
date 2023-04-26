1e0f9d9288a3bcf78dee6c0cce47623bf3369f44
SkipDream <118962764+SkipDream@users.noreply.github.com>
2023-04-26 16:58:24 +0300

    Update Material.cs

--- ClassLibrary1/Material.cs
+++ ClassLibrary1/Material.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* https://docs.google.com/document/d/1YxpUHX84ZBY6UILHctzRWywnt7bWSSup4JTn6Or4p-k/edit */
+
 namespace ClassLibrary1
 {
     public class Material
