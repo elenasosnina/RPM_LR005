22eb2f19da4946b09b1ad4299108037b5b032eaa
Jambo2 <egor.zaytsev.2018@list.ru>
2023-05-01 21:12:58 +0300

    Fix#5

--- Cons1/Creator.cs
+++ Cons1/Creator.cs
@@ -31,7 +31,7 @@ internal class ClassCreator
         Console.WriteLine("Введите дату рождения: ");
         DateTime date = CreateDateTime();
 
-        return new Student(name,surname, patronimyc, Group(), date);
+        return new Student(name, surname, patronimyc, Group(), date);
     }
 
     static DateTime CreateDateTime()
@@ -96,31 +96,43 @@ internal class ClassCreator
 
     public static Group Group()
     {
-            Console.Write("Введите название группы: ");
-            string name = Console.ReadLine() ?? "";
-            Console.Write("Введите сокращённое название группы: ");
-            string sokr = Console.ReadLine() ?? "";
-            Console.Write("Введите численность группы: ");
-            byte population = Convert.ToByte(Console.ReadLine() ?? "25");
-            Console.Write("Введите год поступления группы: ");
-            ushort year = Convert.ToUInt16(Console.ReadLine());
+        Console.Write("Введите название группы: ");
+        string name = Console.ReadLine() ?? "";
+        Console.Write("Введите сокращённое название группы: ");
+        string sokr = Console.ReadLine() ?? "";
+        Console.Write("Введите численность группы: ");
+        byte population = Convert.ToByte(Console.ReadLine() ?? "25");
+        Console.Write("Введите год поступления группы: ");
+        ushort year = Convert.ToUInt16(Console.ReadLine());
 
-            return new Group(name, sokr, population, year, Speciality(), Teacher());
-
-    }
-        public static Teacher Teacher()
-        {
-            return new Teacher();
-        }
-
-        public static Speciality Speciality()
-        {
-            Console.Write("Введите название специальности: ");
-            string name = Console.ReadLine() ?? "";
-            Console.Write("Введите сокращенное название специальноти: ");
-            string reduction = Console.ReadLine() ?? "";
-            return new Speciality(name, reduction);
+        return new Group(name, sokr, population, year, Speciality(), Teacher());
 
     }
- } 
+    public static Teacher Teacher()
+    {
+        return new Teacher();
+    }
 
+    public static Speciality Speciality()
+    {
+        Console.Write("Введите название специальности: ");
+        string name = Console.ReadLine() ?? "";
+        Console.Write("Введите сокращенное название специальноти: ");
+        string reduction = Console.ReadLine() ?? "";
+        return new Speciality(name, reduction);
+
+    }
+
+    public static Organization Organization()
+    {
+        return new Organization();
+    }
+    public static Corpus Corpus()
+    {
+        Console.Write("Введите название: ");
+        string name = Console.ReadLine() ?? "";
+        Console.Write("Введите адрес: ");
+        string address = Console.ReadLine() ?? "";
+        return new Corpus(name, address, Employee(), Organization());
+    }
+}
