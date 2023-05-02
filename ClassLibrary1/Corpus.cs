642c06b0efdc2e0ac9781c4f85e4d52c70df4978
chinchopachinchopa <118964959+chinchopachinchopa@users.noreply.github.com>
2023-05-02 13:20:52 +0300

    Update Corpus.cs

--- ClassLibrary1/Corpus.cs
+++ ClassLibrary1/Corpus.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* https://docs.google.com/document/d/13_TXjBoKn0TzKMcWP9A6jX2vyQEgcxBc4vY1CfJn9ps/edit */
 namespace ClassLibrary1
 {
     public class Corpus
